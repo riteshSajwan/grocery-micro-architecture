a3069b88d97352be06ab1a355e5958084d9163ae
Ritesh Sajwan <riteshsaj8958@gmail.com>
2025-01-05 11:50:27 +0530

    [WIP] added test cases files for all services with dummy test cases

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,11 @@
+//  service  name
+describe("CustomerService", () => {
+    //  function
+    describe("SignIn", () => {
+      // Scenario we are testing
+      test("validate user inputs", () => {});
+  
+      test("Validate response", async () => {});
+    });
+  });
+  
